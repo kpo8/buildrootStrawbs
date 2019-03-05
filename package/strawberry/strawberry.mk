f7e3520b4eb537b167089a320d07df2e8a200fb0
Kevin O'Neil <kevinoneil43@gmail.com>
2019-03-04 19:21:16 -0500

    clementine too old use strawberry

---
+++ package/strawberry/strawberry.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# strawberry
+#
+################################################################################
+
+STRAWBERRY_VERSION = 0.5.2.53.g384209b
+STRAWBERRY_SOURCE =strawberry-$(STRAWBERRY_VERSION).tar.xz
+STRAWBERRY_SITE = https://builds.strawbs.net/source
+STRAWBERRY_INSTALL_STAGING = YES
+STRAWBERRY_INSTALL_TARGET = YES
+STRAWBERRY_DEPENDENCIES = boost gettext libglib2 taglib qt
+STRAWBERRY_LICENSE_FILES = COPYING
+#STRAWBERRY_CONF_OPTS= 
+
+$(eval $(cmake-package))
