a28cd03eebfacca268344b501a38fb93cfe1d222
Kevin O'Neil <kevinoneil43@gmail.com>
2019-03-19 15:55:49 -0400

    added qt build variables

--- board/raspberrypi/rootfs-overlays/etc/masterBoot.sh
+++ board/raspberrypi/rootfs-overlays/etc/masterBoot.sh
@@ -3,5 +3,8 @@
 # startWifi
 /etc/wifiStartup.sh
 
+# physical display properties
+export QT_QPA_EGLFS_PHYSICAL_WIDTH=520
+export QT_QPA_EGLFS_PHYSICAL_HEIGHT=326
 #start Strawberry
 /usr/bin/strawberry
