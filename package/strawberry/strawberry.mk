7bc69536465d55e22b76c4e4109813a69cb22712
Kevin O'Neil <kevinoneil43@gmail.com>
2019-04-29 17:48:02 -0400

    keep it local

--- package/strawberry/strawberry.mk
+++ package/strawberry/strawberry.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-STRAWBERRY_VERSION = 0.5.2.53.g384209b
-STRAWBERRY_SOURCE =strawberry-$(STRAWBERRY_VERSION).tar.xz
-STRAWBERRY_SITE = https://builds.strawbs.net/source
+#STRAWBERRY_VERSION = 0.5.2.53.g384209b
+STRAWBERRY_SOURCE =strawberry.tar.xz#-$(STRAWBERRY_VERSION).tar.xz
+STRAWBERRY_SITE = file:///${HOME}#https://builds.strawbs.net/source
 STRAWBERRY_INSTALL_STAGING = YES
 STRAWBERRY_INSTALL_TARGET = YES
-STRAWBERRY_DEPENDENCIES = boost gettext libglib2 taglib qt5base protobuf sqlite qt5x11extras gstreamer1 openssl chromaprint
+STRAWBERRY_DEPENDENCIES = boost gettext libglib2 taglib qt5base protobuf sqlite qt5x11extras gstreamer1 openssl chromaprint pulseaudio
 STRAWBERRY_LICENSE_FILES = COPYING
 #STRAWBERRY_CONF_OPTS= 
 
